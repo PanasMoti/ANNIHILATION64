6aa12d38a3e9c574c37bff9c389821737933ffeb
PanasMoti <eliasyhoshen@gmail.com>
2023-04-22 22:24:09 +0300

    documented GameOver state

--- sources/State_GameOver.h
+++ sources/State_GameOver.h
@@ -9,10 +9,24 @@
 #include "gfx/Sprite.h"
 #include "RenderWindow.h"
 
+///@brief a Game Over state that is Derived from the BaseState class
+/*
+ * this state has a simple job :
+ * 1.) show the "GAME OVER" image
+ * 2.) show the animated skull
+ * 3.) show the "press Z to return to menu...." text
+ * 4.) show the player their score
+ * 5.) show the player if they Won or lost
+ * 6.) if the player chooses to go back to the mainmenu then switch the current state to the menu state
+ * */
+
+
 class State_GameOver : public BaseState{
 public:
+    //constructor
     State_GameOver(StateManager* l_stateManager);
 
+    // override functions from the base class
     void OnCreate() override;
     void OnDestroy() override;
     void Activate() override;
@@ -20,6 +34,9 @@ public:
     void Update(float dt) override;
     void Draw() override;
 
+
+    /// a callback function that just returns the player to the main menu screen
+    /// \param l_details unused
     void ReturnToMenu(EventDetails* l_details);
 
 private:
